f598e76f2da19e508bbf2d40418cc55778fd636e
Charles Pereira <charlesti.ptu@gmail.com>
2021-06-08 00:40:00 -0300

    configure fonts and theme provider

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,18 +1,12 @@
-import { ReactNode } from 'react';
+import React from "react";
+import { Text } from "react-native";
 
-import { Text } from 'react-native';
+import { Container } from "./styles";
 
-import { Container } from './styles';
-
-interface HomeProps {
-  children: ReactNode;
-}
-
-function Home({ children }: HomeProps) {
+const Home: React.FC = () => {
   return (
     <Container>
       <Text>Home</Text>
-      {children}
     </Container>
   );
 };
---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+export default {
+  colors: {
+    header: '#1B1B1F',
+
+    backgroundPrimary: '#F4F5F6',
+    backgroundSecundary: '#FFFF',
+
+    text: '#7A7A80',
+    textDetail: '#AEAEB3',
+    title: '#47474D',
+
+    line: '#EBEBF0',
+
+    main: '#DC1637',
+    mainLight: '#FDEDEF',
+    success: '#03B252',
+
+    shape: '#E1E1E8',
+    shapeDark: '#29292E',
+  },
+  fonts: {
+    primary400: 'Inter_400Regular',
+    primary500: 'Inter_500Medium',
+    secundary400: 'Archivo_400Regular',
+    secundary500: 'Archivo_500Medium',
+    secundary600: 'Archivo_600SemiBold',
+  },
+};
