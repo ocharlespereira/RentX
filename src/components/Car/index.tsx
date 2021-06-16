24098f7d8fc010aa63b73303cd1d9e4b521cee68
Charles Pereira <charlesti.ptu@gmail.com>
2021-06-16 01:19:06 -0300

    icon params function

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { RectButtonProps } from 'react-native-gesture-handler';
-import GasolineSvg from '../../assets/gasoline.svg';
 
 import { CarDTO } from '../../dtos/carDTO';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 
 import {
   Container,
@@ -22,6 +22,8 @@ interface CarProps extends RectButtonProps {
 }
 
 const Car: React.FC<CarProps> = ({ data, ...rest }) => {
+  const MotorIcon = getAccessoryIcon(data.fuel_type);
+
   return (
     <Container {...rest}>
       <Details>
@@ -35,7 +37,7 @@ const Car: React.FC<CarProps> = ({ data, ...rest }) => {
           </Rent>
 
           <Type>
-            <GasolineSvg />
+            <MotorIcon />
           </Type>
         </About>
       </Details>
--- src/pages/CarDetails/index.tsx
+++ src/pages/CarDetails/index.tsx
@@ -6,13 +6,7 @@ import ImageSlider from '../../components/ImageSlider';
 import Accessory from '../../components/Accessory';
 import Button from '../../components/Button';
 
-import speedSvg from '../../assets/speed.svg';
-import accelerationSvg from '../../assets/acceleration.svg';
-import forceSvg from '../../assets/force.svg';
-import gasolineSvg from '../../assets/gasoline.svg';
-import exchangeSvg from '../../assets/exchange.svg';
-import peopleSvg from '../../assets/people.svg';
-
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 import { CarDTO } from '../../dtos/carDTO';
 
 import {
@@ -77,7 +71,7 @@ const CarDetails: React.FC = () => {
             <Accessory
               key={accessory.type}
               name={accessory.name}
-              icon={speedSvg}
+              icon={getAccessoryIcon(accessory.type)}
             />
           ))}
         </Accessories>
