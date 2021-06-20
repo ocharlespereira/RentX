877429e0156d76eb7b51c79f45217e44542e32f8
charles.pereira <charlesti.ptu@gmail.com>
2021-06-20 20:03:08 -0300

    spash configure initial screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Button, StyleSheet, Dimensions } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
 
 import { Container } from './styles';
 
+const WIDTH = Dimensions.get('window').width;
+
 const Splash: React.FC = () => {
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 1000,
+            easing: Easing.bezier(0, 0.72, 1, -0.57),
+          }),
+        },
+      ],
+    };
+  });
+
+  const handleAnimationPosition = () => {
+    animation.value = Math.random() * (WIDTH - 100);
+  };
+
   return (
     <Container>
-      <Text>Splash</Text>
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title="Mover" onPress={handleAnimationPosition} />
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
 
 export default Splash;
