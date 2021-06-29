1c5876333d146240dea604a9ebc29a374b831207
charles.pereira <charlesti.ptu@gmail.com>
2021-06-28 23:06:48 -0300

    create new component

---
+++ src/screens/SignUp/SignUpFistStep/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+interface SignUpFistStepProps {}
+
+const SignUpFistStep: React.FC = () => {
+  return (
+    <Container>
+      <Text>SignUpFistStep</Text>
+    </Container>
+  );
+};
+
+export default SignUpFistStep;
---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+interface SignUpProps {}
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Text>SignUp</Text>
+    </Container>
+  );
+};
+
+export default SignUp;
