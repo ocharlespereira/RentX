6cdba37636ef3e68066bcdb794b00eec7e4bf47d
Charles Pereira <charlesti.ptu@gmail.com>
2021-07-04 22:49:47 -0300

    component signupSecond

---
+++ src/screens/SignUp/SignUpSecond/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import BackButton from '../../../components/BackButton';
+import Bullet from '../../../components/Bullet';
+import Input from '../../../components/Input';
+import Button from '../../../components/Button';
+
+import {
+  Container,
+  Header,
+  Steps,
+  Title,
+  SubTitle,
+  Form,
+  FormTitle,
+} from './styles';
+
+interface SignUpSecondProps {}
+
+const SignUpSecond: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  const handleBack = () => {
+    goBack();
+  };
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active={true} />
+              <Bullet />
+            </Steps>
+          </Header>
+
+          <Title>Crie sua{'\n'}conta</Title>
+          <SubTitle>Faça seu cadastro de{'\n'}forma rápida e fácil</SubTitle>
+
+          <Form>
+            <FormTitle>1. dados</FormTitle>
+            <Input icon="user" placeholder="Nome" />
+            <Input
+              icon="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+            />
+            <Input
+              icon="credit-card"
+              placeholder="CNH"
+              keyboardType="numeric"
+            />
+          </Form>
+
+          <Button title="Próximo" />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default SignUpSecond;
