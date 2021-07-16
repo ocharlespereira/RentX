f4b1ec291a21e3b82c0c6ae6a18561b0e5ef7aa1
Charles Pereira <charlesti.ptu@gmail.com>
2021-07-15 23:40:03 -0300

    refecture dto

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from '../screens/Home';
+import CarDetails from '../screens/CarDetails';
+import Scheduling from '../screens/Scheduling';
+import SchedulingDetails from '../screens/SchedulingDetails';
+import Confirmation from '../screens/Confirmation';
+import MyCars from '../screens/MyCars';
+import Splash from '../screens/Splash';
+import SignIn from '../screens/SignIn';
+import SignUpFistStep from '../screens/SignUp/SignUpFistStep';
+import SignUpSecond from '../screens/SignUp/SignUpSecond';
+
+const { Navigator, Screen } = createStackNavigator();
+
+/**
+ * initialRouteName -> seta componente a ser iniciado primeiro
+ * gestureEnabled -> desabilita o gesto voltar do ios
+ */
+
+const StackRoutes: React.FC = () => {
+  return (
+    <Navigator headerMode="none" initialRouteName="SignIn">
+      {/* <Screen name="Splash" component={Splash} /> */}
+      <Screen name="SignIn" component={SignIn} />
+      <Screen
+        name="SignUpFistStep"
+        component={SignUpFistStep}
+        options={{ gestureEnabled: false }}
+      />
+      <Screen
+        name="SignUpSecond"
+        component={SignUpSecond}
+        options={{ gestureEnabled: false }}
+      />
+      <Screen
+        name="Home"
+        component={Home}
+        options={{ gestureEnabled: false }}
+      />
+      <Screen name="MyCars" component={MyCars} />
+      <Screen name="CarDetails" component={CarDetails} />
+      <Screen name="Scheduling" component={Scheduling} />
+      <Screen name="SchedulingDetails" component={SchedulingDetails} />
+      <Screen name="Confirmation" component={Confirmation} />
+    </Navigator>
+  );
+};
+
+export default StackRoutes;
