3bd51fd7f5df63df35f0dffb9d42708a76f4af48
Charles Pereira <charlesti.ptu@gmail.com>
2021-07-18 23:55:36 -0300

    home refecture

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { StatusBar, StyleSheet } from 'react-native';
+import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
-import { Ionicons } from '@expo/vector-icons';
-import { useTheme } from 'styled-components';
-import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  useAnimatedGestureHandler,
-  withSpring,
-} from 'react-native-reanimated';
 
 import Car from '../../components/Car';
 import LoadAnimation from '../../components/LoadAnimation';
@@ -20,56 +11,16 @@ import Logo from '../../assets/logo.svg';
 import api from '../../services/api';
 import { CarDTO } from '../../dtos/carDTO';
 
-import {
-  Container,
-  Header,
-  HeaderContent,
-  TotalCars,
-  CarList,
-  MyCarsButton,
-} from './styles';
-
-const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
+import { Container, Header, HeaderContent, TotalCars, CarList } from './styles';
 
 const Home: React.FC = () => {
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const positionY = useSharedValue(0);
-  const positionX = useSharedValue(0);
-
-  const myCarsbuttonStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: positionX.value },
-        { translateY: positionY.value },
-      ],
-    };
-  });
-
-  const onGestureEvent = useAnimatedGestureHandler({
-    onStart(_, ctx: any) {
-      (ctx.positionX = positionX.value), (ctx.positionY = positionY.value);
-    }, //quando o usuário pressiona o botão para começar a arrastar
-    onActive(event, ctx: any) {
-      positionX.value = ctx.positionX + event.translationX;
-      positionY.value = ctx.positionY + event.translationY;
-    }, //quando o usuário está arrastando
-    onEnd() {
-      positionX.value = withSpring(0);
-      positionY.value = withSpring(0);
-    }, // quando o usuário pressionou e arrastou com animação withSprint
-  });
-
-  const theme = useTheme();
   const { navigate } = useNavigation();
 
   const handleCarDetails = (car: CarDTO) => {
     navigate('CarDetails', { car });
-  };
-
-  const handleOpenMyCars = () => {
-    navigate('MyCars');
   };
 
   useEffect(() => {
@@ -81,12 +32,6 @@ const Home: React.FC = () => {
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
   }, []);
-
-  // useEffect(() => {
-  //   BackHandler.addEventListener('hardwareBackPress', () => {
-  //     return true;
-  //   });
-  // });
 
   return (
     <Container>
@@ -114,38 +59,8 @@ const Home: React.FC = () => {
           )}
         />
       )}
-
-      <PanGestureHandler onGestureEvent={onGestureEvent}>
-        <Animated.View
-          style={[
-            myCarsbuttonStyle,
-            { position: 'absolute', bottom: 13, right: 22 },
-          ]}
-        >
-          <ButtonAnimated
-            onPress={handleOpenMyCars}
-            style={[styles.button, { backgroundColor: theme.colors.main }]}
-          >
-            <Ionicons
-              name="ios-car-sport"
-              size={32}
-              color={theme.colors.shape}
-            />
-          </ButtonAnimated>
-        </Animated.View>
-      </PanGestureHandler>
     </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  button: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default Home;
