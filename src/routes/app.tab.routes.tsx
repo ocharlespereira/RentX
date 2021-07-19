7f442994fed2a6d75af01363b5e51dd4e07a4315
Charles Pereira <charlesti.ptu@gmail.com>
2021-07-19 00:07:45 -0300

    refecture menu tabbar icons apply fill styled

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components';
 
 import Home from '../screens/Home';
 import MyCars from '../screens/MyCars';
 import AppStackRoutes from './app.stack.routes';
+
+import HomeSvg from '../assets/home.svg';
+import CarSvg from '../assets/car.svg';
+import PeopleSvg from '../assets/people.svg';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -13,11 +19,48 @@ const { Navigator, Screen } = createBottomTabNavigator();
  */
 
 const AppTabRoutes: React.FC = () => {
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen name="Home" component={AppStackRoutes} />
-      <Screen name="Profile" component={Home} />
-      <Screen name="MyCars" component={MyCars} />
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.main,
+        inactiveTintColor: theme.colors.textDetail,
+        showLabel: false,
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 78,
+          backgroundColor: theme.colors.backgroundPrimary,
+        },
+      }}
+    >
+      <Screen
+        name="Home"
+        component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Profile"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <CarSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="MyCars"
+        component={MyCars}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <PeopleSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 };
