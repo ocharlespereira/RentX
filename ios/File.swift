85b2b7f9c96978063ef0cc8dcd002c2f5d9a66e6
charles.pereira <charlesti.ptu@gmail.com>
2021-07-23 00:06:35 -0300

    config watermelondb ios

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  rentx
+//
+//  Created by charles.pereira on 22/07/21.
+//
+
+import Foundation
--- ios/rentx-Bridging-Header.h
+++ ios/rentx-Bridging-Header.h
@@ -1,7 +1,4 @@
-import <$(SRCROOT)/../node_modules/react-native/React/RCTBundleURLProvider.h>
-import <$(SRCROOT)/../node_modules/react-native/React/RCTRootView.h>
-import <$(SRCROOT)/../node_modules/react-native/React/RCTViewManager.h>
-import <$(SRCROOT)/../node_modules/react-native/React/RCTBridgeModule.h>
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
 
-// Silence warning
-import "../../node_modules/@nozbe/watermelondb/native/ios/WatermelonDB/SupportingFiles/Bridging.h"
