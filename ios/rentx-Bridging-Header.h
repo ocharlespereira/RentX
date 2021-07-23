e09049c705cff55f52277d5aaef76a51720a3401
charles.pereira <charlesti.ptu@gmail.com>
2021-07-22 23:48:45 -0300

    config watermelondb ios

---
+++ ios/rentx-Bridging-Header.h
@@ -0,0 +1,7 @@
+import <$(SRCROOT)/../node_modules/react-native/React/RCTBundleURLProvider.h>
+import <$(SRCROOT)/../node_modules/react-native/React/RCTRootView.h>
+import <$(SRCROOT)/../node_modules/react-native/React/RCTViewManager.h>
+import <$(SRCROOT)/../node_modules/react-native/React/RCTBridgeModule.h>
+
+// Silence warning
+import "../../node_modules/@nozbe/watermelondb/native/ios/WatermelonDB/SupportingFiles/Bridging.h"
