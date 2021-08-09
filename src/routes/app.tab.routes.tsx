337141656febb6e853a23d02df194722b5f1eac9
Charles Pereira <charlesti.ptu@gmail.com>
2021-08-09 18:29:56 -0300

    component profile

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -10,6 +10,7 @@ import AppStackRoutes from './app.stack.routes';
 import HomeSvg from '../assets/home.svg';
 import CarSvg from '../assets/car.svg';
 import PeopleSvg from '../assets/people.svg';
+import Profile from '../screens/Profile';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -45,7 +46,7 @@ const AppTabRoutes: React.FC = () => {
       />
       <Screen
         name="Profile"
-        component={Home}
+        component={Profile}
         options={{
           tabBarIcon: ({ color }) => (
             <CarSvg width={24} height={24} fill={color} />
