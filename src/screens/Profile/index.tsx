f491aaca46f220693b2551e2e9c38805f879087b
Charles Pereira <charlesti.ptu@gmail.com>
2021-08-09 18:39:59 -0300

    component profile

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+import { Feather } from '@expo/vector-icons';
 
-// import { Container } from './styles';
+import BackButton from '../../components/BackButton';
+
+import {
+  Container,
+  Header,
+  HeaderTop,
+  HeanderTitle,
+  LogoutButton,
+  PhotoContainer,
+  Photo,
+} from './styles';
 
 const Profile: React.FC = () => {
-  return <View />;
+  const theme = useTheme();
+  const { goBack } = useNavigation();
+
+  const handleSinOut = () => {};
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTop>
+          <BackButton color={theme.colors.shape} onPress={() => goBack()} />
+          <HeanderTitle>Editar Perfil</HeanderTitle>
+          <LogoutButton onPress={handleSinOut}>
+            <Feather name="power" size={24} color={theme.colors.shape} />
+          </LogoutButton>
+        </HeaderTop>
+
+        <PhotoContainer>
+          <Photo source={{ uri: 'https://github.com/charlespereira1.png' }} />
+        </PhotoContainer>
+      </Header>
+    </Container>
+  );
 };
 
 export default Profile;
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
+export const Container = styled.View``;
 
-`;
+export const Header = styled.View``;
+
+export const HeaderTop = styled.View``;
+
+export const HeanderTitle = styled.Text``;
+
+export const LogoutButton = styled(RectButton)``;
+
+export const PhotoContainer = styled.View``;
+
+export const Photo = styled.Image``;
