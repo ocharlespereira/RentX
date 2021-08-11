e87628a8e39632c4b937ab5136feecf4cfc1c1c7
Charles Pereira <charlesti.ptu@gmail.com>
2021-08-10 23:29:34 -0300

    component profile

--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,16 +1,51 @@
 import styled from 'styled-components/native';
-import { RectButton } from 'react-native-gesture-handler';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.backgroundPrimary};
+`;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+  width: 100%;
+  height: 227px;
 
-export const HeaderTop = styled.View``;
+  background-color: ${({ theme }) => theme.colors.header};
 
-export const HeanderTitle = styled.Text``;
+  padding: 0 24px;
+  align-items: center;
+`;
 
-export const LogoutButton = styled(RectButton)``;
+export const HeaderTop = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 
-export const PhotoContainer = styled.View``;
+  margin-top: ${getStatusBarHeight() + 32}px;
+`;
 
-export const Photo = styled.Image``;
+export const HeanderTitle = styled.Text`
+  font-size: ${RFValue(25)}px;
+  font-family: ${({ theme }) => theme.fonts.secundary600};
+  color: ${({ theme }) => theme.colors.backgroundSecundary};
+`;
+
+export const LogoutButton = styled(BorderlessButton)``;
+
+export const PhotoContainer = styled.View`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+
+  background-color: ${({ theme }) => theme.colors.shape};
+  margin-top: 48px;
+`;
+
+export const Photo = styled.Image`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+`;
