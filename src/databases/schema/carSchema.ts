6987cd0eda31e7706b4657b946e9cbd47bc546db
Charles Pereira <charlesti.ptu@gmail.com>
2021-08-18 00:03:08 -0300

    database cars

---
+++ src/databases/schema/carSchema.ts
@@ -0,0 +1,37 @@
+import { tableSchema } from '@nozbe/watermelondb';
+
+const carSchema = tableSchema({
+  name: 'cars',
+  columns: [
+    {
+      name: 'name',
+      type: 'string',
+    },
+    {
+      name: 'brand',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      type: 'string',
+    },
+    {
+      name: 'fuel_type',
+      type: 'string',
+    },
+    {
+      name: 'period',
+      type: 'string',
+    },
+    {
+      name: 'price',
+      type: 'string',
+    },
+    {
+      name: 'tambnail',
+      type: 'string',
+    },
+  ],
+});
+
+export { carSchema };
